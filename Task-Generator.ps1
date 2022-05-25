e23ad0dcd257a16be6565172039d437d91e04a5e
Gil Shwartz <58849439+GShwartz@users.noreply.github.com>
2022-05-26 00:43:07 +0300

    Add files via upload

---
+++ Task-Generator.ps1
@@ -0,0 +1,20 @@
+﻿# Define script to run
+$file = ".\open.ps1"
+
+# Define Task's Name
+$name = "MyTask"
+
+# Set Task Action
+$action = New-ScheduledTaskAction -Execute "powershell" -Argument "$file" "hello.txt"
+
+# Set Task Trigger
+$trigger = New-ScheduledTaskTrigger -Daily -At 08:00am 
+
+# Register Task
+Register-ScheduledTask -TaskName $name -Action $action -Trigger $trigger
+
+# Disable Task
+Disable-ScheduledTask -TaskName $name
+
+# Display Tasks By Name
+Get-ScheduledTask | where {$_.TaskPath -eq "\"}
---
+++ open.ps1
@@ -0,0 +1,21 @@
+﻿# Open notepad and show Hello World
+
+# Suppress Irrelevant errors
+$ErrorActionPreference = 'silentlycontinue'
+
+# Create hello.txt file with Hello World inside
+$file = $args[0]
+
+# Check if file exists
+$is_file = Test-Path $file -PathType Leaf
+if (-not$is_file) {
+    # Create file
+    $file = New-Item $file
+
+    # Write to file
+    Set-Content $file "Hello World"
+
+}
+
+# Open file with notepad
+start notepad "$file"
